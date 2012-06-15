e28daf646f73097cc7abb5bbb9739af594828a79
Pascal Mächler <1989.pascal@gmail.com>
2012-06-15 03:57:06 +0200

    Add styles and JavaScript to show fields to create new taskgroup to a template

--- Resources/Public/res/JS/tick.js
+++ Resources/Public/res/JS/tick.js
@@ -2,4 +2,7 @@ jQuery(document).ready(function() {
 	jQuery('#showNewTemplateFields').click(function() {
 		jQuery(this).parent().find('#newTemplateForm').show("slow");
 	});
+	jQuery('#showNewTaskGroupFields').click(function() {
+		jQuery(this).parent().find('#newTaskgroupForm').show("slow");
+	});
 });
