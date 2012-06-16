bebeae23916e24077a7ca39429a586acb33edd1f
Pascal Mächler <1989.pascal@gmail.com>
2012-06-16 06:46:14 +0200

    Make new template form, new taskgroup form and new task form reversible

--- Resources/Public/res/JS/tick.js
+++ Resources/Public/res/JS/tick.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function() {
 	jQuery('#showNewTemplateFields').click(function() {
-		jQuery(this).parent().find('#newTemplateForm').show("slow");
+		jQuery(this).parent().find('#newTemplateForm').toggle("slow");
 	});
 	jQuery('#showNewTaskGroupFields').click(function() {
-		jQuery(this).parent().find('#newTaskgroupForm').show("slow");
+		jQuery(this).parent().find('#newTaskgroupForm').toggle("slow");
 	});
 	jQuery('#showNewTaskFields').click(function() {
-		jQuery(this).parent().parent().parent().parent().find('#newTaskForm').show("slow");
+		jQuery(this).parent().parent().parent().parent().find('#newTaskForm').toggle("slow");
 	});
 });
